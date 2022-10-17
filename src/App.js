309ab4c6a5b2830efb845a7cd11f9f56caa06c51
vikas <vkvishwaji@gmail.com>
2022-10-17 16:06:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,284 @@
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+function App() {
+  return (
+    <>
+      
+        <div className="logo">
+          <a href="https://www.indiatoday.in/" target="_blank">
+            <img src="../image/IT-logo.png" width="60px" alt="logo" />
+          </a>
+        </div>
+         
+        <div className="dropdown share-img ">
+          <img
+            src="../image/Share-white.png"
+            className="btn btn-secondary dropdown-toggle"
+            href="#"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+            width="35px"
+            alt="logo"
+          />
+          <ul className="dropdown-menu  opacity-100">
+            <li>
+              <a className="dropdown-item" href="www.facebook.com">
+                f
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="www.whatsapp.com">
+                w
+              </a>
+            </li>
+            <li>
+              <a className="dropdown-item" href="www.twiter.com">
+                twiter
+              </a>
+            </li>
+          </ul>
+        </div>
+      
+       <div className="container">
+      <h1 className="fs-4 h-1">'Yaad Ayenge Yeh Pal': Remembering KK</h1>
+      <div className="t-1">
+        <p className="fs-6 text-center">
+          Kunnath's leap of faith to switch cities would eventually bless
+          Indians with one of <br />
+          the most lovable singers of the ‘90s.
+        </p>
+      </div>
+      <div className="text-ceenter">
+        <img
+          src="../image/pic-1.jpg"
+          width="600px"
+          className="rounded mx-auto d-block"
+          alt="pic"
+        />
+      </div>
+      <div className="t-1">
+        <p className="fs-6 text-center">
+          <span>‘Zara se dil mein de jagah tu…’</span> <br />
+          In 1994, Delhi resident Krishnakumar Kunnath packed his bags and
+          shifted to Bombay. His pregnant wife Jyothy <br /> Krishna accompanied
+          him as the couple looked forward to the husband establishing a career
+          as a singer.
+        </p>
+      </div>
+      <div className="img-fluid img-1">
+        <img
+          src="../image/pic-2.jpg"
+          width="600px"
+          className="rounded mx-auto d-block"
+          alt="pic"
+        />
+      </div>
+      <div className="t-1">
+        <p className="fs-6 text-center">
+          <span>
+            
+            ‘Aayi aisi raat hai jo bohot khushnaseeb hai. Chahe jise door se
+            duniya, woh mere kareeb hai.’
+          </span>
+          <br />
+          <br />
+          <p className="text-center">
+            Two months after the couple reached Bombay, Kunnath found himself in
+            the studio of music composer Lesle Lewis.
+          </p>
+          <p className="text-center">
+            Lesle, who kept a slim Hulk Hogan moustache and a ponytail, had
+            invited Kunnath to his newly-built studio <br /> Purple Haze on the
+            recommendation of music partner Hariharan.
+          </p>
+          <p>
+            Beyond composing music for films, Lewis was also producing jingles
+            for advertisements during that period.
+          </p>
+          <br />
+          <p>
+            After finishing recording for a jingle, and not really sure if Lewis
+            had liked his voice, Kunnath went to the <br /> washroom. He could
+            hear Lewis speak outside to someone on the phone. He heard Lewis say
+            '..he is damn good..."s
+          </p>
+          <br />
+          Kunnath was sure they were talking about him; he punched the air in
+          joy. He had got the break he wanted.
+        </p>
+      </div>
+      <br />
+      <div className="img-1">
+        <img
+          src="../image/pic-3.jpg"
+          width="600px"
+          className="rounded mx-auto d-block"
+          alt="pic"
+        />
+      </div>
+      <br />
+      <div className="t-1">
+        <p className="text-center">
+          <span>
+            ‘Sajde kiye hai lakhon, lakhon duayein maangi, paya hai maine phir
+            tujhe.’
+          </span>{" "}
+          <br />
+          In the next few years, Kunnath would not just sing 3,500 jingles in 11
+          different languages but also some of the <br /> biggest hits Indians
+          would hum.
+        </p>
+      </div>
+      <br />
+      <div className="img-1">
+        <img
+          src="../image/pic-4.jpg"
+          width="600px"
+          className="rounded mx-auto d-block"
+          alt="pic"
+        />
+      </div>
+
+      <div className="t-1">
+        <p className="text-center">
+          <span>‘Kaise rahega bhala, mujhse hokey juda?’</span> <br />
+          In 1996, Vishal Bhardwaj, who was then a music director, got Kunnath
+          to sing a portion of the song 'Chhod Aaye <br /> Hum' in Gulzar's
+          Maachis. Three years later, Kunnath would hit stardom he never
+          imagined.
+        </p>
+      </div>
+
+      <div className="img-1">
+        <img
+          src="../image/pic-5.jpg"
+          width="600px"
+          className="rounded mx-auto d-block"
+          alt="pic"
+        />
+      </div>
+
+      <div className="t-1">
+        <p className="text-center">
+          <span>‘Pal, yeh hai pyaar ke paal.’</span> <br />
+          <br />
+          <p>
+            Sony Music International had just entered the Indian market and was
+            looking for a debut artiste. With mentor <br /> Lesle Louis by his
+            side, Kunnath launched his debut album 'Pal' with Sony Music in
+            1999.
+          </p>
+          <br />
+          <br />
+          And, India got her KK. <br /> The songs 'Pal' and 'Yaaron' from KK's
+          album struck the chord with Indians, especially the college-going
+          youth. An <br /> entire generation would grow up singing the songs.
+        </p>
+      </div>
+
+      <div className="img-1">
+        <img
+          src="../image/pic-6.jpg"
+          width="600px"
+          className="rounded mx-auto d-block"
+          alt="pic"
+        />
+      </div>
+      <br />
+      <div className="t-1">
+        <p className="text-center">
+          <span>‘Jism mujhe de ke mitti ka, sheeshe sa dil kyon banaya?’</span>{" "}
+          <br />
+          <br />
+          KK's journey continued. He sang songs for films and also performed in
+          concerts. He belted out many more hits, <br /> including 'Tadap Tadap'
+          from Hum Dil De Chuke Sanam,'Kya Mujhe Pyaar Hai' from Woh Lamhe,
+          'Aankhon Mein <br /> Teri' from Om Shanti Om, ‘Zara Sa’ from Jannat.
+        </p>
+      </div>
+
+      <div className="img-1">
+        <img
+          src="../image/pic-7.jpg"
+          width="600px"
+          className="rounded mx-auto d-block"
+           
+          alt="pic"
+        />
+      </div>
+      <br />
+      <div className="t-1">
+        <p className="text-center">
+          <span>
+            ‘Dard mein bhi yeh labh muskura jaate hai, beete lamhein hamein jab
+            bhi yaad aate hai.’
+          </span>{" "}
+          <br />
+          KK sang all types of song, ensuring he wouldn't be typecast. He kept
+          up with the changes in the music industry.
+        </p>
+      </div>
+      <br />
+      <div className="img-1">
+        <img
+          src="../image/pic-8.jpg"
+          width="600px"
+          className="rounded mx-auto d-block"
+          alt="pic"
+        />
+      </div>
+      <br />
+      <div className="t-1">
+        <p className="text-center">
+          <span>‘Tujhko main kar lu haasil, lagi hai yahi dhun.’</span> <br />
+          <br />
+          As an individual, KK maintained a humble outlook despite the success.
+          A family guy, he'd value his privacy and <br /> candidly described
+          himself as 'asocial'.
+        </p>
+      </div>
+
+      <div className="img-1">
+        <img
+          src="../image/pic-9.jpg"
+          width="600px"
+          className="rounded mx-auto d-block"
+           
+          alt="pic"
+        />
+      </div>
+      <br />
+      <div className="t-1">
+        <p className="text-center">
+          <span>‘Kal mil jaye, toh hogi, khush naseebi.’</span> <br />
+          Hours before he passed away, he did what he loved the most. He was
+          performing on stage. He sang the song <br /> 'Pal' one last time. The
+          truth in the words ‘Hum rahe ya na rahe kal, kal yaad ayenge yeh pal,’
+          hit home the same <br /> night.
+        </p>
+      </div>
+      </div>
+
+      <div className="container shadow p-3 mb-5 bg-body rounded">
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+          <button
+            type="button"
+            class="btn-close btn-close-white"
+            aria-label="Close"
+          >
+            ×
+          </button>
+        </div>
+        <header className="head">
+          <h1 className="opacity-100">Credits</h1>
+          <hr />
+          <p>Creative Director: Rahul Gupta</p>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default App;
